49d977e6a351d64ddeb6c3a1325bd8c08008cdf3
Jiatao Geng <jiataogeng7@gmail.com>
2025-04-05 21:26:20 -0400

    Update api endpoints to interact with database

--- backend/routes/estimates.js
+++ backend/routes/estimates.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import estimateResalePrice from '../utils/estimator.js';
+import { getEstimates, createEstimate } from '../utils/database.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({msg: "Get all estimates"});
+router.get('/', async (req, res) => {
+    const estimates = await getEstimates();
+    res.status(200).json({estimates});
 });
 
-router.post('/new', (req, res) => {
+router.post('/new', async (req, res) => {
     const {brand, category, originalPrice} = req.body
 
     let missingFields = []
@@ -29,7 +31,8 @@ router.post('/new', (req, res) => {
     }
 
     const estimatedPrice = estimateResalePrice(brand, category, originalPrice);
-    return res.status(200).json({estimatedPrice});
+    const estimate = await createEstimate(brand, category, originalPrice, estimatedPrice);
+    return res.status(200).json(estimate);
 });
 
 export default router;
