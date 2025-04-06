39a0dcef219c30a7fc79f2f4e7f077df7b40efd2
Jiatao Geng <jiataogeng7@gmail.com>
2025-04-05 22:40:40 -0400

    Implement fetch from backend api

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -4,12 +4,29 @@ export default function Form () {
     const [error, setError] = useState(null)
 
     async function handleSubmit(formData) {
+        //Get user inputs from form
         const brand = formData.get('brand')
         const category = formData.get('category')
         const originalPrice = Number(formData.get('original-price'))
-        console.log(brand, category, originalPrice)
+
+        //Fetch
+        const response = await fetch(`http://localhost:4000/api/estimates/new`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({brand, category, originalPrice})
+        })
+        const result = await response.json()
+        console.log(result)
+
+        //Check if response is okay
+        if(!response.ok) {
+            setError(result.error)
+            console.log(error)
+        } else {
+            setError(null)
+        }
     }
-    
+
     return (
         <form className="form" action={handleSubmit}>
             <h3>Estimate Resale Price</h3>
@@ -17,12 +34,12 @@ export default function Form () {
             <input id="brand" type="text" autoComplete="on" name="brand" placeholder="Nike"/>
             <label htmlFor="category">Category</label>
             <select id="category" name="category">
-                <option value="none" className="text-sm">Select a Category</option>
-                <option value="shoes" className="text-sm">Shoes</option>
+                <option value="none">Select a Category</option>
+                <option value="shoes">Shoes</option>
                 <option value="jackets">Jackets</option>
-                <option value="jackets">Sweaters</option>
+                <option value="sweaters">Sweaters</option>
                 <option value="shirts">Shirts</option>
-                <option value="pants" className="text-sm">Pants</option>
+                <option value="pants">Pants</option>
                 <option value="dresses">Dresses</option>
                 <option value="shirts">Other</option>
             </select> 
