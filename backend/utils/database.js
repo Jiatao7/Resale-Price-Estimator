9769cec04655832d1a295e91a3ee7395117741a3
Jiatao Geng <jiataogeng7@gmail.com>
2025-04-05 21:14:11 -0400

    Implement database and functions to get and create estimates

---
+++ backend/utils/database.js
@@ -0,0 +1,34 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//Create database connection
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+}).promise();
+
+export async function getEstimates() {
+    const [rows] = await pool.query("SELECT * FROM estimates")
+    return rows
+}
+
+export async function getEstimate(id) {
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM estimates
+    WHERE id = ?
+    `, [id])
+    return rows[0]
+}  
+
+export async function createEstimate(brand, category, original_price) {
+    const [result] = await pool.query(`
+        INSERT INTO estimates (brand, category, original_price)
+        VALUES (?, ?, ?)
+    `, [brand, category, original_price])
+    const id = result.insertId
+    return getEstimate(id)
+}
